aecd0b288891fcdbffdfaf28a562bcfa4c703f38
Harsh <harshdigwani@gmail.com>
2020-07-04 14:13:54 +0530

    footer issue resolve

--- src/Routes.js
+++ src/Routes.js
@@ -8,6 +8,12 @@ import Categories from './components/Category/Categories';
 import Blogs from './components/Blog/Blogs';
 import CreateBlog from './components/Blog/CreateBlog';
 import Blog from './components/Blog/Blog';
+import Projects from './components/Project/Projects';
+import CreateProject from './components/Project/CreateProject';
+import Project from './components/Project/Project';
+import Profiles from './components/Profile/Profiles';
+import Profile from './components/Profile/Profile';
+import UpdateProfile from './components/Profile/UpdateProfile';
 
 const history = createBrowserHistory();
 
@@ -16,12 +22,23 @@ function Routes() {
     <Router history={history}>
       <Switch>
         <Route exact path="/" component={App} />
+
         <Route exact path="/signup" component={Signup} />
         <Route exact path="/signin" component={Signin} />
+
         <Route exact path="/categories" component={Categories} />
+
         <Route exact path="/blogs" component={Blogs} />
         <Route exact path="/createblog" component={CreateBlog} />
         <Route exact path="/blog/:id" component={Blog} />
+
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/createproject" component={CreateProject} />
+        <Route exact path="/project/:id" component={Project} />
+
+        <Route exact path="/profiles" component={Profiles} />
+        <Route exact path="/updateProfile" component={UpdateProfile} />
+        <Route exact path="/profile/:id" component={Profile} />
       </Switch>
     </Router>
   )
